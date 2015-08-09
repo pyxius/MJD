23a82b573a9ec7654bd52b75b855ec7bbc745364
satya-prakash-23 <satyaprakash23@gmail.com>
2015-08-09 10:54:35 -0500

    initial checkin

---
+++ src/main/java/com/pyxius/mjd/App.java
@@ -0,0 +1,13 @@
+package com.pyxius.mjd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
